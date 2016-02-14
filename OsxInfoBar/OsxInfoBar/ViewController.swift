d4bc16c2ba3b292864560fb99a9cb63b668b3840
Andy Calderbank <andy@plumhead-software.com>
2016-02-14 13:34:10 +0000

    Blank Cocoa Project

---
+++ OsxInfoBar/OsxInfoBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OsxInfoBar
+//
+//  Created by Plumhead on 14/02/2016.
+//  Copyright © 2016 Andy Calderbank. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
